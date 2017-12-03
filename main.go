324b1b4482946a73b1805d2accf23e19209b5d7a
Eric Liao <rcliao01@gmail.com>
2017-12-02 17:41:32 -0800

    chore: update cat example

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"html/template"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
@@ -13,7 +16,7 @@ import (
 
 type comment struct {
 	Name    string
-	Comment string
+	Comment template.HTML
 }
 
 func main() {
@@ -77,12 +80,53 @@ func createComment(db *sql.DB) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		username := r.FormValue("username")
 		comment := r.FormValue("comment")
-		_, err := db.Query("INSERT INTO comments (name, comment) VALUES ('" + username + "', '" + comment + "')")
+		comment = strings.Replace(
+			comment,
+			"cat",
+			fmt.Sprintf("<img src=\"%s\">", getRandomCatPicture()),
+			-1,
+		)
+		_, err := db.Query(
+			"INSERT INTO comments (name, comment) VALUES (?, ?)",
+			username,
+			comment,
+		)
 		if err != nil {
 			panic(err)
 		}
 		http.Redirect(w, r, "/", 302)
 	})
+}
+
+type giphyResp struct {
+	Data giphyData `json:"data"`
+}
+type giphyData struct {
+	ImageURL string `json:"image_url"`
+}
+
+func getRandomCatPicture() string {
+	// https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cat
+	resp, err := http.Get("https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cat")
+	if err != nil {
+		fmt.Println("Failed to get response from Github", err)
+		return ""
+	}
+	defer resp.Body.Close()
+	b, err := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(b))
+	if err != nil {
+		fmt.Println("Failed to read body from Github get token response", err)
+		return ""
+	}
+	var respBody giphyResp
+	err = json.Unmarshal(b, &respBody)
+	if err != nil {
+		fmt.Println("Failed to parse json body from Github get token response", err)
+		return ""
+	}
+	return respBody.Data.ImageURL
+
 }
 
 func static() http.Handler {
